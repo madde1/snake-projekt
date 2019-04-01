77efa38b0d6ac56ea4e802fc61a16be96d4d2e10
Wictor Sundström <wictor.sundstrom@hotmail.com>
2019-04-01 19:25:36 +0200

    changed and cleaned up the code

--- php/pongHighscore.php
+++ php/pongHighscore.php
@@ -1,14 +1,16 @@
 <?php
-//ta bort lösenordet
-    $link = mysqli_connect("localhost", "root", "pussel91", "highscore");
+
+    $connection = mysqli_connect("localhost", "root", "", "highscore") or die("Could not connect");;
+    mysqli_select_db($connection, "highscore") or die ("Could not select database");
+
+    $data = array();
 
     if (mysqli_connect_error()){
         die ("Error! Can't connect");
     }
 
     $query = 'SELECT * FROM pong';
-    $result = mysqli_query($link,$query);
-
+    $result = mysqli_query($connection,$query);
 
     while($row = mysqli_fetch_assoc($result))
     {
--- php/snakeHighscore.php
+++ php/snakeHighscore.php
@@ -1,22 +1,21 @@
 <?php
-//ta bort lösenordet
-    $link = mysqli_connect("localhost", "root", "pussel91", "highscore");
+
+    $connection = mysqli_connect("localhost", "root", "", "highscore") or die("Could not connect");
+    mysqli_select_db($connection, "highscore") or die ("Could not select database");
+
     $data = array();
 
-if (mysqli_connect_error()){
+    if (mysqli_connect_error()){
         die ("Error! Can't connect");
     }
 
     $query = 'SELECT * FROM snake';
-    $result = mysqli_query($link,$query);
+    $result = mysqli_query($connection,$query);
 
     while($row = mysqli_fetch_assoc($result))
     {
         $data[] = $row;
     }
     echo json_encode ( $data );
-
-            //while($row = mysqli_fetch_assoc($result)){
-            //echo $row["snakeName"]. " " .$row["snakeScore"];
 
 ?>
