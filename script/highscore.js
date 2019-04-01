56810e5fdee046ab6e92b0ab5e62303d23b656f5
Wictor Sundström <wictor.sundstrom@hotmail.com>
2019-04-01 19:23:36 +0200

    slipt highscore in two

--- script/highscore.js
+++ script/highscore.js
@@ -1,69 +1,14 @@
-let xmlhttp, data, url, newContent = "";
-let snakeHighscoreV = [];
-let pongHighscoreV = [];
+let xmlhttp, pongData, url, snakeData, snakeContent = "", pongContent = "";
 
-document.getElementById("snakeButton").addEventListener("click", function() {
-    newContent = '';
-    xmlhttp = new XMLHttpRequest();
-    url = 'php/snakeHighscore.php';
+var sort_by = function(field, reverse, primer){
 
+    var key = primer ?
+        function(x) {return primer(x[field])} :
+        function(x) {return x[field]};
 
-    xmlhttp.open("GET", url , true);
-    xmlhttp.send();
+    reverse = !reverse ? 1 : -1;
 
-    xmlhttp.onreadystatechange = function()
-    {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText);
-
-            for (let y in data) {
-                snakeHighscoreV[y] = data[y].snakeScore;
-
-                newContent += '<li>';
-                newContent += data[y].snakeName;
-                newContent += ' ';
-                newContent += data[y].snakeScore;
-                newContent += '</li>';
-            }
-            console.log(snakeHighscoreV[2]);
-            document.getElementById("snakeHighscoreList").innerHTML = newContent;
-        }
+    return function (a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
     }
-});
-
-document.getElementById("pongButton").addEventListener("click", function() {
-    newContent = '';
-
-    xmlhttp = new XMLHttpRequest();
-    url = 'php/pongHighscore.php';
-
-
-    xmlhttp.open("GET", url , true);
-    xmlhttp.send();
-
-    xmlhttp.onreadystatechange = function()
-    {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.responseText);
-
-            for (let y in data) {
-                pongHighscoreV[y] = data[y].pongScore;
-
-                newContent += '<li>';
-                newContent += data[y].pongName;
-                newContent += ' ';
-                newContent += data[y].pongScore;
-                newContent += '</li>';
-            }
-
-            console.log(pongHighscoreV[2]);
-            document.getElementById("pongHighscoreList").innerHTML = newContent;
-        }
-    }
-});
-
-function checkPongHighscore(score) {
-    if(score > pongHighscoreV){
-
-    }
-}
+};
