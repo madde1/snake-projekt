ae4ce00347ef94432aa9698692e12bac53288906
Wictor Sundstrom <wictor.sundstrom@hotmail.com>
2019-04-02 22:11:30 +0200

    Moved code to help prevent 502 error

--- php/pongUpdate.php
+++ php/pongUpdate.php
@@ -2,12 +2,12 @@
     include("connection.php");
 
     $stmt = $connection->prepare("UPDATE pong SET pongName=?, pongScore=? WHERE pongId=?");
-    $stmt->bind_param("sii", $name, $score, $id);
 
     $name = $_POST['name'];
     $score = $_POST['score'];
     $id = $_POST['id'];
 
+    $stmt->bind_param("sii", $name, $score, $id);
     $stmt->execute();
 
 ?>
--- php/snakeUpdate.php
+++ php/snakeUpdate.php
@@ -1,14 +1,13 @@
 <?php
     include("connection.php");
 
-
     $stmt = $connection->prepare("UPDATE snake SET snakeName=?, snakeScore=? WHERE snakeId=?");
-    $stmt->bind_param("sii", $name, $score, $id);
 
     $name = $_POST['name'];
     $score = $_POST['score'];
     $id = $_POST['id'];
 
+    $stmt->bind_param("sii", $name, $score, $id);
     $stmt->execute();
 
 ?>
